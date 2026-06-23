12ae003877f6c71875da95430ff0cda378d1ff87
voltstimesamps <268468425+voltstimesamps@users.noreply.github.com>
2026-06-23 16:14:35 -0700

    Add dimensions and weight columns to system schema

---
+++ backend/prisma/migrations/20260623231333_add_dimensions_and_weight_columns/migration.sql
@@ -0,0 +1,7 @@
+-- AlterTable
+ALTER TABLE "System" ADD COLUMN     "depthMm" INTEGER,
+ADD COLUMN     "lengthMm" INTEGER,
+ADD COLUMN     "screenResolutionX" INTEGER,
+ADD COLUMN     "screenResolutionY" INTEGER,
+ADD COLUMN     "weightKg" DOUBLE PRECISION,
+ADD COLUMN     "widthMm" INTEGER;
